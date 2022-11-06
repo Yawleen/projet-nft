a09dbec1c803d46fd23bb47ebe50f92999602767
mguydefontgalland <mariaguydefontgalland@gmail.com>
2022-11-06 18:28:12 +0100

    (refactor:) dynamic url to fetch informations of a NFT

--- product/index.js
+++ product/index.js
@@ -1,4 +1,12 @@
 // Récupération des informations d'un NFT en particulier 
-fetch('https://awesome-nft-app.herokuapp.com/nft/385541166')
+
+// Récupération de l'ID du NFT grâce au lien 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const nftID = urlParams.get('id'); 
+//const nftID = 385541166; // Pour les tests
+console.log(nftID);
+
+fetch('https://awesome-nft-app.herokuapp.com/nft/' + nftID)
   .then((response) => response.json())
   .then((data) => console.log(data));
