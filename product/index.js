d253a446da6d2a9f312a1b0c9531bc264e5394e6
mguydefontgalland <mariaguydefontgalland@gmail.com>
2022-11-06 18:20:29 +0100

    (refactor:) API request to fetch details about a NFT

--- product/index.js
+++ product/index.js
@@ -1,1 +1,4 @@
-console.log('test');
+// Récupération des informations d'un NFT en particulier 
+fetch('https://awesome-nft-app.herokuapp.com/nft/385541166')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
