7936ffead531476ab4cb1660f0471c52507fbc7c
mguydefontgalland <mariaguydefontgalland@gmail.com>
2022-11-08 21:58:40 +0100

    use of a promise for the api request and added some structure to the code
    (style: editing commit message)

--- product/index.js
+++ product/index.js
@@ -1,12 +1,28 @@
-// Récupération des informations d'un NFT en particulier 
+let apiRequest = new Promise((resolve, reject) => {
+    // Récupération de l'ID du NFT grâce au lien 
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    //const nftID = urlParams.get('id'); 
+    const nftID = 385541166; // Pour les tests
+    console.log(nftID);
+    // Récupération des informations d'un NFT en particulier en fonction du ID du NFT
+    fetch('https://awesome-nft-app.herokuapp.com/nft/' + nftID).then(response => response.json())
+        .then(function (data) {
+            //console.log(data);
+            resolve(data); 
+        });
+})
 
-// Récupération de l'ID du NFT grâce au lien 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const nftID = urlParams.get('id'); 
-//const nftID = 385541166; // Pour les tests
-console.log(nftID);
+async function displayNFTDetails(){
+    let nftInfos  = await apiRequest; 
+    console.log(nftInfos); 
 
-fetch('https://awesome-nft-app.herokuapp.com/nft/' + nftID)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+    // Intégration des informations du NFT dans le template HTML 
+    const nftTitle = document.getElementById('nft_title');
+    console.log(nftTitle);
+}
+
+displayNFTDetails();
+
+
+
