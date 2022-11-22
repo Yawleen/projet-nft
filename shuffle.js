38cc7287348f619d2d36d911fd7d988a5a26fb6a
“Aline <hy.aline@outlook.com>
2022-11-23 00:39:04 +0100

    (fix:) making sure the shuffle doesn't generate duplicates

--- shuffle.js
+++ shuffle.js
@@ -7,40 +7,28 @@ const btnShuffle = document.querySelector(".shuffle-option");
 
 // Méthode qui permet de requêter l'api et récupérer des données + qui permet de récupérer trois nft random
 
-getRandom();
-
-async function getRandom() {
-    
-
+const getRandom = async () => {
+  try {
     const response = await fetch(URL);
     const data = await response.json();
     const keys = data.assets;
+    const nodeElements = document.querySelectorAll('.shuffle-card');
+    const arrayElements = [...nodeElements];
+    shuffle(0, keys.length, keys);
 
-    for (let i = 0; i < 3; i++) {
-
-        const nodeElements = document.querySelectorAll('.shuffle-card');
-        const arrayElements = [...nodeElements];
-
-        if(arrayElements.length > 3) {
-            shuffleZone.innerHTML = '';
-            for (let i = 0; i < 2; i++) {
-                const randomIndex = Math.floor(Math.random() * keys.length);
-                const randomKey = keys[randomIndex];
-                displayNFT(randomKey);
-            } 
-        } else {
-        randomIndex = Math.floor(Math.random() * keys.length);
-        randomKey = keys[randomIndex];
-
-        displayNFT(randomKey);
-        }
+    if (arrayElements.length >= 3) {
+      shuffleZone.innerHTML = '';
+      shuffle(0, keys.length, keys); 
     }
-}
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 function displayNFT(nft) {
-    const element = document.createElement('div');
-        element.classList.add('shuffle-card');
-        element.innerHTML = `<div class="product-card">
+  const element = document.createElement('div');
+  element.classList.add('shuffle-card');
+  element.innerHTML = `<div class="product-card">
         <div class="product-image">
           <img src="${nft.image_url}" alt="nft ${nft.name}">
         </div>
@@ -55,21 +43,31 @@ function displayNFT(nft) {
                 nft.creator.username.slice(1)
             }</p>`
           }
-          ${
-            nft.description &&
-            `<p class="product-description"><span>Description</span> ${
-                nft.description[0].toUpperCase() + nft.description.slice(1)
-            }</p>`
-          }
-          <p class="product-sales"><span>Nombre de ventes</span> ${
-            nft.sales
-          }</p>
           <button class="more-info" onclick="window.open('../product/index.html?id=${
             nft.id
           }', '_blank')">Plus d'info</button>
         </div>
         </div>`;
 
-        shuffleZone.append(element);
+  shuffleZone.append(element);
 
 }
+
+function shuffle(min, max, array) {
+  let num, already = new Object;
+
+  let tab = array;
+  let start = min;
+  let end = max;
+
+      for (let i = 0; i < 3;) {
+        num = (Math.random() * (end - start) + start) ^ 0;
+        if (!(num in already)) {
+          already[num] = num;
+          i++;
+          displayNFT(tab[num]);
+        }
+      }
+}
+
+getRandom();
